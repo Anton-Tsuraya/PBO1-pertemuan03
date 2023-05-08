9b6515fe4ef0e9dc567796ebe050c7df0126484d
Anton <129922565+Anton-Tsuraya@users.noreply.github.com>
2023-05-08 16:59:40 +0800

    Create OperatorBinary.java

---
+++ pertemuan03/src/main/java/Operator/OperatorBinary.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Operator;
+
+/**
+ *
+ * @author ANTON TSURAYA
+ */
+public class OperatorBinary {
+    String nama = "Anton Tsuraya";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+    
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
+}
