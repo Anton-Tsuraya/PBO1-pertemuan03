089b3dd5b9a6daf67695d6a6b12eddd41d39a994
Anton <129922565+Anton-Tsuraya@users.noreply.github.com>
2023-05-08 17:34:51 +0800

    Create Switch.java

---
+++ pertemuan03/src/main/java/Percabangan/Switch.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Percabangan;
+
+/**
+ *
+ * @author ANTON TSURAYA
+ */
+public class Switch {
+    public static void main(String[] args) {
+        int jalanJalan=3;
+        switch(jalanJalan){
+            case 1:
+                System.out.println("urang lapas");
+                break;
+            case 2:
+                System.out.println("urang acayan");
+                break;
+            case 3:
+                System.out.println("ubat nyamuk");
+                break;
+            case 4:
+                System.out.println("dubil dit");
+                break;
+            case 5:
+                System.out.println("mahluk halus");
+                break;
+        }
+    }
+}
