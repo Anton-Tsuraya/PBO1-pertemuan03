bd351292242ecbf8ed6122f0c6b928822f78f2c3
Anton <129922565+Anton-Tsuraya@users.noreply.github.com>
2023-05-08 17:04:25 +0800

    Create OpratorUnary.java

---
+++ pertemuan03/src/main/java/Operator/OpratorUnary.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Operator;
+
+/**
+ *
+ * @author ANTON TSURAYA
+ */
+public class OpratorUnary {
+    public static void main(String[] args) {
+        int i = 0;
+        i++;
+        System.out.println(i);
+        i++;
+        System.out.println(i);
+        i=i+1;
+        System.out.println(i);
+    }    
+}
